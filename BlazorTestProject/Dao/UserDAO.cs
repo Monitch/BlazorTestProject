599e150a548950dd15c7850c2c702fd1526ef8b2
monic <monich17v@gmail.com>
2021-07-29 11:53:13 +0300

    Conected db, maded ddd, delete show function

--- BlazorTestProject/Dao/UserDAO.cs
+++ BlazorTestProject/Dao/UserDAO.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorTestProject.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,37 +9,81 @@ namespace BlazorTestProject.Dao
 {
     public class UserDAO
     {
-        private int idIncrement;
-        private List<User> people;
-        public UserDAO()
-        {
-            people = new List<User>();
-            people.Add(new User { Id = ++idIncrement, Name = "Vitaliy Monich", Email = "Monich@gmail.com", Number = "1111" });
-            people.Add(new User { Id = ++idIncrement, Name = "Vasyl Luck", Email = "vlook@test.com", Number = "2222" });
-            people.Add(new User { Id = ++idIncrement, Name = "Dovbaka Mukola", Email = "Azur@test.com", Number = "3333" });
-            people.Add(new User { Id = ++idIncrement, Name = "Denis Vakar", Email = "vakarov@test.com", Number = "4444" });
-        }
+        static string connectionString = "Data Source=DESKTOP-3D3OK6O\\MSQL;Initial Catalog=contact;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        private List<User> people = new List<User>();
+
         public List<User> takeList()
         {
+            try
+            {
+                connection.Open();
+                string sqlExpression = string.Format("SELECT * FROM [User]");
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader number = command.ExecuteReader();
+                if (number.HasRows)
+                {
+                    while (number.Read())
+                    {
+                        User user = new User();
+                        user.Id = number.GetInt32(0);
+                        user.Name = number.GetString(1);
+                        user.Number = number.GetString(2);
+                        user.Email = number.GetString(3);
+                        people.Add(user);
+                    }
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
             return people;
         }
         public void save(User contact)
         {
-            contact.Id = ++idIncrement;
-            people.Add(contact);
+            string sqlExpression = String.Format("INSERT INTO [User] (Name, Number, Email) VALUES ('{0}', '{1}', '{2}')",
+                contact.Name,contact.Number,contact.Email);
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
         public void remove(int id)
         {
-            int index = 0;
-            foreach (var user in people)
+            string sqlExpression = String.Format("DELETE FROM [User] WHERE id='{0}'",id);
+            try
             {
-                if (user.Id == id)
-                {
-                    break;
-                }
-                index++;
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();
+
             }
-            people.RemoveAt(index);
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
         }
         public void updateContact(int id, User contact)
         {
